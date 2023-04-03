ffb0843b622a482cc7a2ca9e1296b45815101669
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-04-02 19:30:48 -0700

    complete half of main content

---
+++ js/images.js
@@ -0,0 +1,90 @@
+let imagesCollection = [{
+    link:'https://unsplash.com/photos/qtRF_RxCAo0',
+    name:'universe-background-image',
+    alt:'universe',
+    websiteOrigin:'https://unsplash.com',
+    relatedArticle:{
+        text:'',
+        author:'',
+        publishedOn:''
+    },
+    belongsTo:'Aldebaran S ',
+    src:'',
+    placement:'Home'
+},
+{
+    link:'https://unsplash.com/photos/rCbdp8VCYhQ',
+    name:'home-splash',
+    alt:'universeOn',
+    websiteOrigin:'https://unsplash.com/',
+    relatedArticle:{
+        text:'',
+        author:'',
+        publishedOn:''
+    },
+    belongsTo:'Andy Holmes',
+    src:'',
+    placement:'Home-Top'
+},
+{
+    link:'https://unsplash.com/photos/F-pSZO_jeE8',
+    name:'eclipse',
+    alt:'Solar Eclipse',
+    websiteOrigin:'https://unsplash.com',
+    relatedArticle:{
+        text:'Last year, We drove to Kentucky from NJ. It was long drive to go like 15 hours, and took more than 20 hours to came back home. But it was worth it. Amazing event of my life. Never forget but at least I can share with you all.',
+        author:'Jongsun Lee',
+        publishedOn:''
+    },
+    belongsTo:'',
+    src:'',
+    placement:'',
+    imageDate:'June 23, 2018'
+},
+{
+    link:'https://www.pexels.com/photo/man-doing-crossfit-6389075/',
+    name:'gym-workout',
+    alt:'gym crossfit',
+    websiteOrigin:'https://www.pexels.com',
+    relatedArticle:{
+        text:'',
+        author:'',
+        publishedOn:''
+    },
+    belongsTo:'Tima Miroshnichenko',
+    src:'',
+    placement:'',
+    imageDate:''
+},
+{
+    link:'https://pixabay.com/photos/man-exercise-fitness-gym-dumbbells-2604149/',
+    name:'fitness',
+    alt:'fitness-workout',
+    websiteOrigin:'https://pixabay.com',
+    relatedArticle:{
+        text:'',
+        author:'',
+        publishedOn:''
+    },
+    belongsTo:'StockSnap',
+    src:'',
+    placement:'under the fitness section',
+    imageDate:''
+},
+{
+    link:'',
+    name:'',
+    alt:'',
+    websiteOrigin:'',
+    relatedArticle:{
+        text:'',
+        author:'',
+        publishedOn:''
+    },
+    belongsTo:'',
+    src:'',
+    placement:'',
+    imageDate:''
+}];
+
+let headingText = "The universe is everything. It includes all of space, and all the matter and energy that space contains. It even includes time itself and, of course, it includes you.";
